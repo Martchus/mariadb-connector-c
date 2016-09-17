cd68269a531ce4e600f3a99d1687219e0959af84
Martchus <martchus@gmx.net>
2016-09-17 16:51:15 +0200

    Use fopen rather than fopen_s for XP compatibility

--- libmariadb/dbug.c
+++ libmariadb/dbug.c
@@ -2051,11 +2051,7 @@ static void DBUGOpenFile(CODE_STATE *cs,
       }
       else
       {
-#ifdef _WIN32
-        if (fopen_s(&fp, name, append ? "a+" : "w"))
-#else
         if (!(fp= fopen(name, append ? "a+" : "w")))
-#endif
         {
           (void) fprintf(stderr, ERR_OPEN, cs->process, name);
           perror("");
